fb5268574e66f68ab09e98d590a6d63f06e9f4c3
leesper <pascal7718@gmail.com>
2017-05-04 16:43:41 +0800

    add test

---
+++ yht_test.go
@@ -0,0 +1,29 @@
+package goyht
+
+import (
+	"testing"
+)
+
+func TestAddUser(t *testing.T) {}
+
+func TestModifyPhoneNumber(t *testing.T) {}
+
+func TestModifyUserName(t *testing.T) {}
+
+func TestUserToken(t *testing.T) {}
+
+func TestCreateTemplateContract(t *testing.T) {}
+
+func TestCreateFileContract(t *testing.T) {}
+
+func TestAddPartner(t *testing.T) {}
+
+func TestSignContract(t *testing.T) {}
+
+func TestInvalidateContract(t *testing.T) {}
+
+func TestListContracts(t *testing.T) {}
+
+func TestLookupContractDetail(t *testing.T) {}
+
+func TestDownloadContract(t *testing.T) {}
